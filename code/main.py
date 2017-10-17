4698d80a99e86687b3f31a1fa2092fe3fe1e58e1
Guoxin SUI <suiguoxin@gmail.com>
2017-10-17 21:45:32 +0800

    BASELINE: realise data preprocessing and logistic regression model

---
+++ code/main.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+import pandas as pd  # 数据分析
+import numpy as np  # 科学计算
+from pandas import Series, DataFrame
+import sklearn.preprocessing as preprocessing
+
+from utile import set_missing_ages, set_Cabin_type
+
+data_train = pd.read_csv("./../data/train.csv")
+
+# data pre-processing
+# rfr to fill the missing data ??????????????
+data_train, rfr = set_missing_ages(data_train)
+data_train = set_Cabin_type(data_train)
+
+# 特征因子化
+dummies_Cabin = pd.get_dummies(data_train['Cabin'], prefix= 'Cabin')
+dummies_Embarked = pd.get_dummies(data_train['Embarked'], prefix= 'Embarked')
+dummies_Sex = pd.get_dummies(data_train['Sex'], prefix= 'Sex')
+dummies_Pclass = pd.get_dummies(data_train['Pclass'], prefix= 'Pclass')
+
+df = pd.concat([data_train, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+
+
+# scaling
+# add .reshape(-1, 1) to fix compilation error
+# delete the age and fare rows ??????????
+scaler = preprocessing.StandardScaler()
+age_scale_param = scaler.fit(df['Age'].reshape(-1, 1))
+df['Age_scaled'] = scaler.fit_transform(df['Age'].reshape(-1, 1), age_scale_param)
+fare_scale_param = scaler.fit(df['Fare'].reshape(-1, 1))
+df['Fare_scaled'] = scaler.fit_transform(df['Fare'].reshape(-1, 1), fare_scale_param)
+
+# train
+from sklearn import linear_model
+
+# 用正则取出我们要的属性值
+train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+train_np = train_df.as_matrix()
+
+# y即Survival结果
+y = train_np[:, 0]
+
+# X即特征属性值
+X = train_np[:, 1:]
+
+# fit到RandomForestRegressor之中
+clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+clf.fit(X, y)
+
+
+# test data pre-processing
+data_test = pd.read_csv("./../data/test.csv")
+data_test.loc[ (data_test.Fare.isnull()), 'Fare' ] = 0
+# 接着我们对test_data做和train_data中一致的特征变换
+# 首先用同样的RandomForestRegressor模型填上丢失的年龄
+tmp_df = data_test[['Age','Fare', 'Parch', 'SibSp', 'Pclass']]
+null_age = tmp_df[data_test.Age.isnull()].as_matrix()
+# 根据特征属性X预测年龄并补上
+X = null_age[:, 1:]
+predictedAges = rfr.predict(X)
+data_test.loc[ (data_test.Age.isnull()), 'Age' ] = predictedAges
+
+data_test = set_Cabin_type(data_test)
+dummies_Cabin = pd.get_dummies(data_test['Cabin'], prefix= 'Cabin')
+dummies_Embarked = pd.get_dummies(data_test['Embarked'], prefix= 'Embarked')
+dummies_Sex = pd.get_dummies(data_test['Sex'], prefix= 'Sex')
+dummies_Pclass = pd.get_dummies(data_test['Pclass'], prefix= 'Pclass')
+
+
+df_test = pd.concat([data_test, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+df_test.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+df_test['Age_scaled'] = scaler.fit_transform(df_test['Age'].reshape(-1, 1), age_scale_param)
+df_test['Fare_scaled'] = scaler.fit_transform(df_test['Fare'].reshape(-1, 1), fare_scale_param)
+
+# prediction
+test = df_test.filter(regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+predictions = clf.predict(test)
+result = pd.DataFrame({'PassengerId':data_test['PassengerId'].as_matrix(), 'Survived':predictions.astype(np.int32)})
+result.to_csv("./../data/logistic_regression_predictions.csv", index=False)
